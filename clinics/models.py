13efb52621945152334b8d6de9edff475324ecaf
Javokhir <kasimovjavohir01@icloud.com>
2022-05-20 18:57:46 +0500

    New migrations and search fields

--- clinics/models.py
+++ clinics/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 
 class Clinic(models.Model):
+    WEEK_DAYS = [
+        ('Пн', 'Понедельник'),
+        ('Вт', 'Вторник'),
+        ('Ср', 'Среда'),
+        ('Чт', 'Четверг'),
+        ('Пт', 'Пятница'),
+        ('Сб', 'Суббота'),
+        ('Вс', 'Воскресенье'),
+    ]
     title = models.CharField(max_length=250, null=True)
     image = models.ImageField(upload_to='clinic_photo', null=True)
     phone_num = models.CharField(max_length=15, null=True)
@@ -12,6 +21,12 @@ class Clinic(models.Model):
     lat_long = models.CharField(max_length=255, null=True)
     open_time = models.TimeField()
     close_time = models.TimeField()
+    start_day = models.CharField(null=True, max_length=25, choices=WEEK_DAYS)
+    end_day = models.CharField(null=True, max_length=25, choices=WEEK_DAYS)
 
     def __str__(self):
         return self.title
+
+    @property
+    def address(self):
+        return f"{self.region}, {self.district}, {self.street} {self.home_num}"
--- clinics/views.py
+++ clinics/views.py
@@ -1,8 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .serializers import ClinicSerializer
 from .models import Clinic
 
 
 class ClinicsListView(generics.ListAPIView):
+    filter_backends = [filters.SearchFilter]
     serializer_class = ClinicSerializer
     queryset = Clinic.objects.all()
+    search_fields = [
+        'title',
+        'region',
+        'district',
+        'street'
+    ]
